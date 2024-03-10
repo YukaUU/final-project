e88fc48f530a2b1c80cf53f3b28f58e685aea8f6
YukaUU <usuidayuu@icloud.com>
2024-03-10 12:00:45 +0900

    構成：新しいファイルを追加しました

---
+++ src/app/Modal/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material";
+
+export default function About() {
+  return (
+    <>
+    <div>
+      <h2>モーダル</h2>
+    </div>
+    <Button variant="contained">Hello World</Button>
+  </>
+  );
+}
---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material";
+
+export default function About() {
+  return (
+    <>
+    <div>
+      <h2>薬追加</h2>
+    </div>
+    <Button variant="contained">Hello World</Button>
+  </>
+  );
+}
