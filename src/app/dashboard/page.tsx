0d2a0a05ce684daf77dcc0a147106830cf89bbb4
YukaUU <usuidayuu@icloud.com>
2024-03-10 11:26:31 +0900

    カレンダー作成

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,13 @@
+import { DateCalendar } from '@mui/DateCalendar';
+import Button from '@mui/material/Button'; 
+
 export default function About() {
     return (
+      <>
       <div>
         <h2>こんにちは from DashBoard</h2>
       </div>
+      <Button variant="contained">Hello World</Button>
+    </>
     );
   }
